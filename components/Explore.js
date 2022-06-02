543c7221e030162e4f8f7438f85a43bd49a8a031
marolabio <marolabio@gmail.com>
2022-06-02 22:09:44 +0800

    updated explore images to be clickable and have alt

--- components/Explore.js
+++ components/Explore.js
@@ -2,6 +2,7 @@ import { Box, Container, Grid, styled, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import MenuList from '@mui/material/MenuList';
+import ModalImage from 'react-modal-image';
 
 const ExploreContainer = styled(Container)({
   height: '100%',
@@ -18,8 +19,8 @@ const exploreData = [
     details:
       'Satisfy your cravings with the delicious Filipino and Western food exclusive in our restaurant',
     images: [
-      { img: '/images/restaurant1.jpg' },
-      { img: '/images/restaurant2.jpg' },
+      { img: '/images/restaurant1.jpg', title: 'river village restaurant' },
+      { img: '/images/restaurant2.jpg', title: 'river village restaurant' },
     ],
   },
   {
@@ -28,8 +29,8 @@ const exploreData = [
     details:
       'Relax and enjoy the view on top with picturesque river, ocean and sunset.',
     images: [
-      { img: '/images/treehouse1.jpg' },
-      { img: '/images/treehouse2.jpg' },
+      { img: '/images/treehouse1.jpg', title: 'river village treehouse' },
+      { img: '/images/treehouse2.jpg', title: 'river village treehouse' },
     ],
   },
   {
@@ -38,8 +39,8 @@ const exploreData = [
     details:
       'Dine or bring your own food in our cottages while watching the fishes',
     images: [
-      { img: '/images/cottages1.jpg' },
-      { img: '/images/cottages2.jpg' },
+      { img: '/images/cottages1.jpg', title: 'river village cottages' },
+      { img: '/images/cottages2.jpg', title: 'river village cottages' },
     ],
   },
   {
@@ -47,20 +48,29 @@ const exploreData = [
     name: 'Kayaking',
     details:
       'Feel the excitement and  thrill of diving, swimming and kayaking!',
-    images: [{ img: '/images/kayak1.jpg' }, { img: '/images/kayak2.jpg' }],
+    images: [
+      { img: '/images/kayak1.jpg', title: 'river village kayaking' },
+      { img: '/images/kayak2.jpg', title: 'river village kayaking' },
+    ],
   },
   {
     id: 4,
     name: 'Fishing and Fish Feeding',
     details:
       'Experience the village life. Tip: You can take away your fresh catch or have it cooked in our restaurant',
-    images: [{ img: '/images/fishing1.jpg' }, { img: '/images/fishing2.jpg' }],
+    images: [
+      { img: '/images/fishing1.jpg', title: 'river village bangus fish' },
+      { img: '/images/fishing2.jpg', title: 'river village fishing' },
+    ],
   },
   {
     id: 5,
     name: 'Camping',
     details: 'Be one with nature. Unplug and enjoy the simplicity of nature.',
-    images: [{ img: '/images/camping1.jpg' }, { img: '/images/camping2.jpg' }],
+    images: [
+      { img: '/images/camping1.jpg', title: 'river village camping' },
+      { img: '/images/camping2.jpg', title: 'river village camping' },
+    ],
   },
 ];
 
@@ -139,14 +149,20 @@ function Explore({ myRef }) {
                   },
                 }}
               >
-                <Box
+                {/* <Box
                   sx={{
                     background: `url(${data.images[0].img}) no-repeat`,
                     backgroundSize: 'contain',
                     width: '100%',
                     paddingTop: '80%',
                   }}
-                ></Box>
+                ></Box> */}
+                <ModalImage
+                  large={data.images[0].img}
+                  small={data.images[0].img}
+                  alt={data.images[0].alt}
+                  width='100%'
+                />
               </Box>
             </Grid>
             <Grid
@@ -173,14 +189,20 @@ function Explore({ myRef }) {
                 <Typography component='p' variant='body1' mb={2} mt={2}>
                   {data.details}
                 </Typography>
-                <Box
+                {/* <Box
                   sx={{
                     background: `url(${data.images[1].img}) no-repeat`,
                     backgroundSize: 'contain',
                     width: '100%',
                     paddingTop: '65%',
                   }}
-                ></Box>
+                ></Box> */}
+                <ModalImage
+                  large={data.images[1].img}
+                  small={data.images[1].img}
+                  alt={data.images[1].alt}
+                  width='100%'
+                />
               </Box>
             </Grid>
           </Grid>
