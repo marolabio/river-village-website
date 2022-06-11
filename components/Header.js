b965dfc218d8d577f6b3d2d2dae114a8ab81d852
marolabio <marolabio@gmail.com>
2022-06-11 21:01:04 +0800

    optimize header image with next image

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,9 @@
 import { Button, Box, Container, Typography, styled } from '@mui/material';
+import Image from 'next/image';
 
 const HeroBackground = styled(Box)({
   background: "url('/images/hero.png') center center/cover no-repeat",
-  height: '90vh',
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
+
   boxShadow: 'inset 0, 0, 0, 1000px rgba(0, 0, 0, 0.2)',
   objectFit: 'contain',
   color: '#FFF',
@@ -14,13 +11,35 @@ const HeroBackground = styled(Box)({
 
 const Header = ({ executeScroll }) => {
   return (
-    <HeroBackground>
+    <Box sx={{ position: 'relative', color: '#FFf' }}>
+      <Image
+        src='/images/hero.png'
+        alt='river village resort logo'
+        layout='fill'
+        objectFit='cover'
+      />
       <Container>
-        <Box>
-          <Typography variant='h2' component='h1'>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            height: '90vh',
+            width: '100%',
+          }}
+        >
+          <Typography
+            sx={{ position: 'relative', zIndex: 1 }}
+            variant='h2'
+            component='h1'
+          >
             Enjoy your stay in Bolinao
           </Typography>
-          <Typography variant='subtitle1' component='h2'>
+          <Typography
+            sx={{ position: 'relative', zIndex: 1 }}
+            variant='subtitle1'
+            component='h2'
+          >
             Eat delicious food, Relax and Experience village life
           </Typography>
           <Box mt={3}>
@@ -36,7 +55,7 @@ const Header = ({ executeScroll }) => {
           </Box>
         </Box>
       </Container>
-    </HeroBackground>
+    </Box>
   );
 };
 
